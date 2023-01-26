1290ed81fdd7ab2671464e69bdd1a1e7a24da286
dharmey <ankur31d@gmail.com>
2023-01-26 20:14:07 +0530

    coin listing done, coin details done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CoinsList from "./pages/CoinsList";
+import CoinDetails from "./pages/CoinDetails";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CoinsList />} />
+          <Route path="/coin/:id" element={<CoinDetails />} />
+          <Route path="*" element={<CoinsList />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
